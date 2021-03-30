0d62dd46d042c474f03087298358747957546676
中瑋 蔡 <107111248@mail.oit.edu.tw>
2021-03-30 14:49:31 +0800

    閏年判斷

--- 2-2Practice/Default.aspx.cs
+++ 2-2Practice/Default.aspx.cs
@@ -8,7 +8,15 @@ using System.Web.UI.WebControls;
 namespace _2_2Practice {
     public partial class Default : System.Web.UI.Page {
         protected void Page_Load(object sender, EventArgs e) {
-
+            int year = 1996;
+            if (year % 400 == 0 || (year % 4 == 0 && year % 100 != 0))
+            {
+                Response.Write(year + "年是閏年");
+            }
+            else
+            {
+                Response.Write(year + "年不是閏年");
+            }
         }
     }
 }
